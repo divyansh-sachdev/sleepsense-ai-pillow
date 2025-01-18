0c35630f7f351c7cba8562009cbb3c2861abb391
Divyansh Sachdev <89719842+divyansh-sachdev@users.noreply.github.com>
2025-01-18 10:30:00 +0530

    Initial commit: Piezoelectric matrix driver and ADC multiplexer logic

---
+++ config.example.h
@@ -0,0 +1,12 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#define PRESSURE_PIN_L    34
+#define PRESSURE_PIN_C    35
+#define PRESSURE_PIN_R    39
+#define MIC_ANALOG_PIN    36
+
+#define SNORE_THRESHOLD   650
+#define APNEA_TIME_LIMIT  10000 // ms
+
+#endif
